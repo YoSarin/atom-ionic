5971285c961b80d332b7227b7703115f5c81ecc4
Martin <martin@yosarin.net>
2017-12-31 01:11:56 +0100

    initial commit

---
+++ spec/atom-ionic-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomIonicView from '../lib/atom-ionic-view';
+
+describe('AtomIonicView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
